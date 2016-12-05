fe483169646a39009fc4bc8de3a2c60e3fde9216
Zach Connelly <zmconnelly@gmail.com>
2016-12-05 09:14:04 -0700

    because compile

--- src/State.java
+++ src/State.java
@@ -1,13 +1,14 @@
 
 public class State {
-    
-    
 
     public Position position;
     public int x = position.x, y = position.y;   //Agent's location
     public int Vx = 0, Vy = 0; //Agent's velocity
-    
-    public State(Position position, int vX, int vY){
+
+    public State() {
+    }
+
+    public State(Position position, int vX, int vY) {
         this.position = position;
         Vx = vX;
         Vy = vY;
