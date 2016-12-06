7f02d318a7f7bd6cf9255bce937e446dce85278a
Zach Connelly <zmconnelly@gmail.com>
2016-12-05 20:08:55 -0700

    changes to Velocity

--- src/Velocity.java
+++ src/Velocity.java
@@ -16,13 +16,19 @@ public class Velocity {
         }
     }
 
-    private final Pair<Integer, Integer> velocity;
+    private final int x;
+    private final int y;
 
-    public Velocity(int xA, int yA) {
-        this.velocity = new Pair(xA, yA);
+    public Velocity(int Vx, int Vy) {
+
+        assert (Vx >= -5 && Vx <= 5) : "Invalid X velocity.";
+        assert (Vy >= -5 && Vy <= 5) : "Invalid Y velocity.";
+
+        this.x = Vx;
+        this.y = Vy;
     }
 
-    public Pair<Integer, Integer> getVelocity() {
-        return velocity;
+    public int[] getVelocity() {
+        return new int[]{x, y};
     }
 }
