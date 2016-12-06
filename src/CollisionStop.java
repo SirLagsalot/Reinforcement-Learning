bcffbb6be0fa63f564ed5635cc9278756e2ac722
Ryan Brand <UNA8949@gmail.com>
2016-12-06 10:01:58 -0700

    Collision stop returns agent to closest position to start position but needs the actual board to check the surrounding spaces. Or modify bresenham's to return the list of checked spaces

--- src/CollisionStop.java
+++ src/CollisionStop.java
@@ -6,6 +6,22 @@ public class CollisionStop implements ICollisionHandler {
     public State handleCollision(State startState, Position collisionPosition) {
         State state = new State();
         //TODO, find prev square from startState and collisionPosition and return it with Velocity 0;
+        double shortest = 9999;
+        for (int i = -1; i < 2; i++) {
+            for (int j = -1; j < 2; j++) {
+                if (j != 0 && i != 0) {
+                    double temp = distance(startState.x, startState.y, collisionPosition.x + j, collisionPosition.y + i);
+                    if (temp < shortest) {
+                        shortest = temp;
+                    }
+                }
+            }
+        }
+
         return state;
+    }
+
+    public double distance(int x0, int y0, int x1, int y1) {
+        return Math.sqrt((Math.pow((double) (x1 - x0), 2)) + (Math.pow((double) (y1 - y0), 2)));
     }
 }
