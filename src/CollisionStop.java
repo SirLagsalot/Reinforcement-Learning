cf53533dea6fc241595e8da802e3ab35034bdfb3
Definitely Not Wilson <zmconnelly@gmail.com>
2016-12-07 13:05:58 -0700

    stuff

--- src/CollisionStop.java
+++ src/CollisionStop.java
@@ -1,13 +1,9 @@
-//Returns to agents previous position with velocity set to 0
 
-//If we pass the previous position to the handler, all we should have to do is retrieve the correct state for that position with velocity 0, right?
+//Returns to agents previous position with velocity set to 0
 public class CollisionStop implements ICollisionHandler {
 
     @Override
     public State handleCollision(State startState, Position prevPos) {
-
-        return new State(prevPos, new Velocity(0, 0));   
-//im not sure, but i think we just want to set the agent back to its previous spot and 0 its velocity?  This still isnt quite how we want to do this since we dont want to instantiate new state, but should work for now? maybe
-
+        return new State(prevPos, new Velocity(0, 0));
     }
 }
--- src/GUI.java
+++ src/GUI.java
@@ -1,13 +1,12 @@
 
 import java.awt.Frame;
 
-
 public class GUI extends Frame {
-    
+
     public GUI() {
-        
+
     }
-    
+
     public void renderTrack(char[][] track, Position agentLoc) {
         //TODO: make it so
     }
