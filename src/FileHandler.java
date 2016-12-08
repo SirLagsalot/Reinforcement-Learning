d6fce87f57fe74e65680ea0ae1b3e158f2d55ccc
Zach Connelly <zmconnelly@gmail.com>
2016-12-08 13:34:20 -0700

    Reading and writing working, i think

--- src/FileHandler.java
+++ src/FileHandler.java
@@ -1,25 +1,19 @@
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class FileHandler {
 
     public static void exportPolicy(double[][] policy, String fileName) {
 
         List<String> lines = new ArrayList<>();
-        for (int i = 0; i < policy.length; i++) {
+        for (double[] p : policy) {
             String line = "";
             for (int j = 0; j < policy[0].length; j++) {
-                line += policy[i][j];
+                line += p[j];
                 if (j < policy[0].length - 1) {
                     line += ",";
                 }
@@ -29,28 +23,27 @@ public class FileHandler {
         try {
             Files.write(Paths.get("policies/" + fileName + ".txt"), lines);
         } catch (IOException ex) {
-            Logger.getLogger(FileHandler.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println(ex);
         }
     }
 
     public static double[][] importPolicy(String fileName) {
 
-        double[][] policy;
-//        try {
-//            FileReader in = new FileReader("policies/" + fileName + ".txt");
-//            BufferedReader reader = new BufferedReader(in);
-//            String line = reader.readLine();
-//            String[] values = line.split(",");
-//            policy = new int[values.length];
-//            for (int i = 0; i < values.length; i++) {
-//                policy[i] = Integer.parseInt(values[i]);
-//            }
-//            return policy;
-//        } catch (IOException | NumberFormatException e) {
-//            System.out.println("Exception caught: " + e);
-//        }
-//        return new int[1];
-//    }
-        return new double[0][0];//policy;
+        try {
+            List<String> lines = Files.readAllLines(Paths.get("policies/" + fileName + ".txt"));
+            double[][] policy = new double[lines.size()][9];
+            for (int i = 0; i < lines.size(); i++) {
+                String[] elements = lines.get(i).split(",");
+                double[] values = new double[elements.length];
+                for (int j = 0; j < elements.length; j++) {
+                    values[j] = Double.parseDouble(elements[j]);
+                }
+                policy[i] = values;
+            }
+            return policy;
+        } catch (IOException ex) {
+            System.out.println(ex);
+        }
+        return new double[0][0];
     }
 }
