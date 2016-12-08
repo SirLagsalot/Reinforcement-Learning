6da4a37eb978117b2dfb70a8588b766fbc5b64fa
WilsonSunBritten <wilsunb@gmail.com>
2016-12-08 13:40:04 -0700

    no good rewards

--- src/QLearner.java
+++ src/QLearner.java
@@ -62,8 +62,8 @@ public class QLearner extends PolicyMaker {
         double endEta = .1;
         double etaKneelingFactor = Math.pow(endEta/eta, 1/(double)totalEpisodes);
         //maybe .9 because there is NO reward until the final state is reached...
-        double gamma = .8;//I guess?
-        double liklihoodToExplore = 1;//start liklihood to explore
+        double gamma = .1;//I guess?
+        double liklihoodToExplore = 1.5;//start liklihood to explore
         double endLiklihoodToExplore = .05;
         double exploreToGreedyKneelingFactor = Math.pow(endLiklihoodToExplore/liklihoodToExplore, 1/(double)totalEpisodes);
         int currentStateID = 0;
@@ -91,7 +91,7 @@ public class QLearner extends PolicyMaker {
                 int reward = -1;
                 StateInfo resultInfo = this.idMap.getStateInfoFromPosition(result.position);
                 if (resultInfo.isFinal) {
-                    reward = 100;
+                    reward = 0;
                 }
                 int newStateID = this.idMap.computeStateIDFromStateAndStateInfo(result, resultInfo);
                 
