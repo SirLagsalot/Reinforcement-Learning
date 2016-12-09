ece26a0fd1dcd55cd6fdf02b87fc54db5a18e9eb
Definitely Not Wilson <zmconnelly@gmail.com>
2016-12-08 21:49:24 -0700

    Some code cleanup

--- src/Softmax.java
+++ src/Softmax.java
@@ -2,18 +2,19 @@
 //Softmax function returns an action from the set of possible actions according to a weighted probability distribution
 public class Softmax {
 
-    private static double temperature = 1.0;     //tunable temperature parameter.  Higher values force greater chance of choosing randomly.  Lower values force selection to become greedy.
+    private static double temperature = 1.0;                            //tunable temperature parameter.  Higher values force greater chance of choosing randomly.  Lower values force selection to become greedy.
 
     public static int getNextAction(double[] qValues) {
 
         //assign probabilities to all potential actions
-        double[] probabilityTable = new double[qValues.length];        //table containing probability that each of the potential accelerations should be chosen.
+        double[] probabilityTable = new double[qValues.length];         //table containing probability that each of the potential accelerations should be chosen.
 
         double sum = 0.0;
         for (double q : qValues) {
             sum += Math.exp(q / temperature);
         }
 
+        //compute probability to choose each action
         for (int i = 0; i < qValues.length; i++) {
             double top = Math.exp(qValues[i] / temperature);
             probabilityTable[i] = top / sum;
