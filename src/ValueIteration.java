579013bf582b850c1a38c6c01731b41e2fb32335
Definitely Not Wilson <zmconnelly@gmail.com>
2016-12-11 12:42:48 -0700

    ValueIteration output, we like?

--- src/ValueIteration.java
+++ src/ValueIteration.java
@@ -1,12 +1,14 @@
 
+import java.text.DecimalFormat;
 import java.util.Random;
 
 public class ValueIteration extends PolicyMaker {
 
     private final double epsilon = 0.00000000000000001;     //stopping threshold
     private final double gamma = 0.5;                       //discount factor 
+    private final double eta = 0.9;
+
     private int numIterations;                              //count of iterations required to build policy
-    private double eta = 0.9;
 
     public ValueIteration(StateIDMapper map, char[][] track, Simulator sim) {
         super(map, track, sim);
@@ -30,64 +32,36 @@ public class ValueIteration extends PolicyMaker {
 
         double bellmanResidual = epsilon * (1 - gamma) / gamma;
         double[] vK = new double[this.idMap.getMaxState() + 1];
+        DecimalFormat df = new DecimalFormat("0.0000");
         System.out.println("Bellman Residual: " + bellmanResidual);
 
         while (true) {
+
+            numIterations++;
             boolean allvkPass = true;
             double[] oldVK = vK;
+            System.out.println("Computing new Vk[] for all states\n");
             double[] newVK = iterate(vK);
             vK = newVK;
-            //System.out.println("Finished iteration: Delta = "+delta);
+
+            System.out.println("Comparing new Vk[] to old Vk");
             for (int i = 0; i < vK.length; i++) {
                 if (Math.abs(newVK[i] - oldVK[i]) >= epsilon) {
-                    System.out.println("the too big vK difference was...: " + Math.abs(newVK[i] - oldVK[i]));
+                    System.out.println("Iteration " + numIterations + ": Delta(Vk) for stateID " + i + ": " + df.format(Math.abs(newVK[i] - oldVK[i])) + " is > residual (" + bellmanResidual + ")");
                     allvkPass = false;
-
+                    System.out.println("Continuing to next iteration.\n");
                     break;
                 }
             }
             if (allvkPass) {
+                System.out.println("ALL Vk < BELLMAN RESIDUAL");
                 break;
             }
-        } //while (delta < bellmanResidual);
-
-        //max v_k into policy: todo
+        }
         return getBestActions(vK);
     }
 
-    private int[] getBestActions(double[] vK) {
-
-        int[] policy = new int[vK.length];
-        for (int stateID = 0; stateID < vK.length; stateID++) {
-
-            double bestResult = -99999;
-            int bestAction = 0;
-
-            for (int i = 0; i < 9; i++) {
-                double actionValue = 0;
-                State state = idMap.GetStateFromID(stateID);
-                State succActionState = simulator.takeActionDetermisistic(state, new Action(i));
-                int succActionReward = -1;
-                if (succActionState.finish == true) {
-                    succActionReward = 0;
-                }
-                State failActionState = simulator.takeActionDetermisistic(state, new Action(4));//should be the 0,0 action...
-                int failActionReward = -1;
-                if (failActionState.finish == true) {
-                    failActionReward = 0;
-                }
-                actionValue += 0.2 * (failActionReward + (eta * vK[idMap.getStateIDFromState(failActionState)]));
-                actionValue += 0.8 * (succActionReward + (eta * vK[idMap.getStateIDFromState(succActionState)]));
-                if (actionValue > bestResult) {
-                    bestResult = actionValue;
-                    bestAction = i;
-                }
-            }
-            policy[stateID] = bestAction;
-        }
-        return policy;
-    }
-
+    //Update expected utility of all states
     private double[] iterate(double[] vK) {
 
         double[] newVK = new double[vK.length];
@@ -123,5 +97,40 @@ public class ValueIteration extends PolicyMaker {
             }
         }
         return bestResult;
+    }
+
+    private int[] getBestActions(double[] vK) {
+
+        int[] policy = new int[vK.length];
+        System.out.println("Comupting best action for each state.");
+        for (int stateID = 0; stateID < vK.length; stateID++) {
+
+            double bestResult = -99999;
+            int bestAction = 0;
+
+            for (int i = 0; i < 9; i++) {
+                double actionValue = 0;
+                State state = idMap.GetStateFromID(stateID);
+                State succActionState = simulator.takeActionDetermisistic(state, new Action(i));
+                int succActionReward = -1;
+                if (succActionState.finish == true) {
+                    succActionReward = 0;
+                }
+                State failActionState = simulator.takeActionDetermisistic(state, new Action(4));    //Don't accelerate
+                int failActionReward = -1;
+                if (failActionState.finish == true) {
+                    failActionReward = 0;
+                }
+                actionValue += 0.2 * (failActionReward + (eta * vK[idMap.getStateIDFromState(failActionState)]));
+                actionValue += 0.8 * (succActionReward + (eta * vK[idMap.getStateIDFromState(succActionState)]));
+                if (actionValue > bestResult) {
+                    bestResult = actionValue;
+                    bestAction = i;
+                }
+            }
+            policy[stateID] = bestAction;
+        }
+        System.out.println("Done building Value Iteration Policy after " + numIterations + " iterations!\n\n");
+        return policy;
     }
 }
